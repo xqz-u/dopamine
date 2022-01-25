a3d80d23da5ab9afa42de97f89d521e0b9344e59
xqz-u <marco.gallo0530@gmail.com>
2022-01-26 00:32:22 +0100

    nightly run

--- dopamine/thesis/tests/test_par_run.py
+++ dopamine/thesis/tests/test_par_run.py
@@ -1,5 +1,6 @@
 import logging
 
+from dopamine.discrete_domains import gym_lib
 from dopamine.jax import losses
 from thesis import exploration
 from thesis.agents import dqv_max
@@ -8,6 +9,14 @@ from thesis.runner import parallel_runs, reporter
 repo = "/home/xqz-u/uni/thesis/resources/data/test_par_runner"
 cartpole_path = "CartPole-DQVMax"
 acrobot_path = "Acrobot-DQVMax"
+cartpole_preprocs = {
+    "min_vals": tuple(gym_lib.CARTPOLE_MIN_VALS),
+    "max_vals": tuple(gym_lib.CARTPOLE_MAX_VALS),
+}
+acrobot_preprocs = {
+    "min_vals": tuple(gym_lib.ACROBOT_MIN_VALS),
+    "max_vals": tuple(gym_lib.ACROBOT_MAX_VALS),
+}
 
 conf_cartpole = {
     "nets": {
@@ -15,6 +24,7 @@ conf_cartpole = {
             "model": {
                 # "call_": ...,
                 "hiddens": (512, 512),
+                **cartpole_preprocs,
             },
             "optim": {
                 # "call_": ...,
@@ -23,7 +33,10 @@ conf_cartpole = {
             "loss": losses.huber_loss,
         },
         "vnet": {
-            "model": {"hiddens": (512, 512)},
+            "model": {
+                "hiddens": (512, 512),
+                **cartpole_preprocs,
+            },
             "optim": {"learning_rate": 0.001},
             "loss": losses.huber_loss,
         },
@@ -72,6 +85,7 @@ conf_acrobot = {
             "model": {
                 # "call_": ...,
                 "hiddens": (512, 512),
+                **acrobot_preprocs,
             },
             "optim": {
                 # "call_": ...,
@@ -80,7 +94,10 @@ conf_acrobot = {
             "loss": losses.huber_loss,
         },
         "vnet": {
-            "model": {"hiddens": (512, 512)},
+            "model": {
+                "hiddens": (512, 512),
+                **acrobot_preprocs,
+            },
             "optim": {"learning_rate": 0.001},
             "loss": losses.huber_loss,
         },
@@ -123,6 +140,6 @@ conf_acrobot = {
     },
 }
 
-# parallel_runs.run_multiple_configs([conf_cartpole, conf_acrobot], 2)
+parallel_runs.run_multiple_configs([conf_cartpole, conf_acrobot], 2)
 # parallel_runs.run_experiment(conf_cartpole)
-parallel_runs.run_experiment(conf_acrobot)
+# parallel_runs.run_experiment(conf_acrobot)
