f5169b65488e85db5df39c128f47a8e0f375461a
xqz-u <marco.gallo0530@gmail.com>
2022-01-25 20:04:04 +0100

    work on other computer

---
+++ dopamine/thesis/runner/parallel_runs.py
@@ -0,0 +1,16 @@
+import multiprocessing as mp
+from typing import List
+
+from thesis.runner import runner
+
+
+def run_experiment(conf: dict):
+    manager = runner.create_runner(conf)
+    manager.run_experiment_with_redundancy()
+
+
+def run_multiple_configs(configs: List[dict], n_workers: int = None):
+    n_configs, cores = len(configs), mp.cpu_count()
+    n_workers = n_workers or (n_configs if n_configs < cores else cores - 1)
+    with mp.Pool(n_workers) as p:
+        p.map(run_experiment, configs)
---
+++ dopamine/thesis/tests/test_par_run.py
@@ -0,0 +1,128 @@
+import logging
+
+from dopamine.jax import losses
+from thesis import exploration
+from thesis.agents import dqv_max
+from thesis.runner import parallel_runs, reporter
+
+repo = "/home/xqz-u/uni/thesis/resources/data/test_par_runner"
+cartpole_path = "CartPole-DQVMax"
+acrobot_path = "Acrobot-DQVMax"
+
+conf_cartpole = {
+    "nets": {
+        "qnet": {
+            "model": {
+                # "call_": ...,
+                "hiddens": (512, 512),
+            },
+            "optim": {
+                # "call_": ...,
+                "learning_rate": 0.001
+            },
+            "loss": losses.huber_loss,
+        },
+        "vnet": {
+            "model": {"hiddens": (512, 512)},
+            "optim": {"learning_rate": 0.001},
+            "loss": losses.huber_loss,
+        },
+    },
+    "exploration": {"call_": exploration.egreedy_linear_decay},
+    "agent": {
+        "call_": dqv_max.DQVMaxAgent,
+        "net_sync_freq": int(1e4),
+        "min_replay_history": int(1e5),
+        "observation_shape": (4, 1),
+    },
+    "env": {
+        # "call_": gym_lib.create_gym_env,
+        "environment_name": "CartPole",
+        "version": "v0",
+        # "clip_rewards": True
+    },
+    "memory": {
+        # "call_": outofgraph...,
+        # "stack_size": 1
+    },
+    "runner": {
+        "base_dir": f"{repo}/{cartpole_path}",
+        # "schedule": "continuous_train_and_eval",
+        "log_level": logging.INFO,
+        # "resume": False,
+        "experiment": {
+            "seed": 4,
+            "steps": 1000,
+            "iterations": 1000,
+            "redundancy": 5,
+        },
+        "reporters": [
+            {
+                "call_": reporter.AimReporter,
+                "repo": repo,
+                "experiment": "CartPole-DQVMax",
+            }
+        ],
+    },
+}
+
+conf_acrobot = {
+    "nets": {
+        "qnet": {
+            "model": {
+                # "call_": ...,
+                "hiddens": (512, 512),
+            },
+            "optim": {
+                # "call_": ...,
+                "learning_rate": 0.001
+            },
+            "loss": losses.huber_loss,
+        },
+        "vnet": {
+            "model": {"hiddens": (512, 512)},
+            "optim": {"learning_rate": 0.001},
+            "loss": losses.huber_loss,
+        },
+    },
+    "exploration": {"call_": exploration.egreedy_linear_decay},
+    "agent": {
+        "call_": dqv_max.DQVMaxAgent,
+        "net_sync_freq": int(1e4),
+        "min_replay_history": int(1e5),
+        "observation_shape": (6, 1),
+    },
+    "env": {
+        # "call_": gym_lib.create_gym_env,
+        "environment_name": "Acrobot",
+        "version": "v1",
+        # "clip_rewards": True
+    },
+    "memory": {
+        # "call_": outofgraph...,
+        # "stack_size": 1
+    },
+    "runner": {
+        "base_dir": f"{repo}/{acrobot_path}",
+        # "schedule": "continuous_train_and_eval",
+        "log_level": logging.INFO,
+        # "resume": False,
+        "experiment": {
+            "seed": 10,
+            "steps": 1000,
+            "iterations": 1000,
+            "redundancy": 5,
+        },
+        "reporters": [
+            {
+                "call_": reporter.AimReporter,
+                "repo": repo,
+                "experiment": "Acrobot-DQVMax",
+            }
+        ],
+    },
+}
+
+# parallel_runs.run_multiple_configs([conf_cartpole, conf_acrobot], 2)
+# parallel_runs.run_experiment(conf_cartpole)
+parallel_runs.run_experiment(conf_acrobot)
