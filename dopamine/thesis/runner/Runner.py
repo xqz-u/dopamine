eb066b8f79961f949b612681f099e0c5a6eeb3b2
xqz-u <marco.gallo0530@gmail.com>
2022-03-22 18:42:39 +0100

    share eval and train_and_eval methods between runners

--- dopamine/thesis/runner/Runner.py
+++ dopamine/thesis/runner/Runner.py
@@ -1,4 +1,5 @@
 import logging
+import operator
 import os
 import pprint
 from abc import ABC, abstractmethod
@@ -240,18 +241,45 @@ class Runner(ABC):
     def train_iteration(self) -> dict:
         pass
 
-    @abstractmethod
-    def eval_iteration(self) -> dict:
-        pass
-
-    @abstractmethod
-    def train_and_eval_iteration(self) -> dict:
-        pass
-
     @property
     @abstractmethod
     def console_name(self):
         pass
+
+    def eval_one_episode(self) -> OrderedDict:
+        ep_reward, ep_steps = 0.0, 0
+        done, observation = False, self.env.reset()
+        while not done:
+            action = self.agent.select_action(observation)
+            observation, reward, done, _ = self.step_environment(action, ep_steps)
+            ep_reward += reward
+            ep_steps += 1
+        return OrderedDict(reward=ep_reward, steps=ep_steps)
+
+    # NOTE default implementations provided, override if necessary
+    def eval_iteration(self) -> dict:
+        self.agent.eval_mode = True
+        eval_info = OrderedDict(reward=0.0, steps=0, episodes=0)
+        while eval_info["steps"] < self.eval_steps:
+            utils.inplace_dict_assoc(
+                eval_info, operator.add, update_dict=self.eval_one_episode()
+            )
+            eval_info["episodes"] += 1
+        aggregate_info = {"AvgEp_return": eval_info["reward"] / eval_info["episodes"]}
+        self.report_metrics(eval_info, aggregate_info)
+        self.agent.eval_mode = False
+        return {"raw": eval_info, "aggregate": aggregate_info}
+
+    def train_and_eval_iteration(self) -> dict:
+        train_dict = self.train_iteration()
+        eval_dict = None
+        if self.curr_iteration % self.eval_period == 0:
+            eval_dict = self.eval_iteration()
+        return (
+            {"train": train_dict}
+            if eval_dict is None
+            else {"train": train_dict, "eval": eval_dict}
+        )
 
 
 # NOTE that e.g. in an iteration of 600 steps, enough experience to
