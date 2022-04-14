65bdf236d80863a3dcb1940b58837f455225f4c1
xqz-u <marco.gallo0530@gmail.com>
2022-04-14 18:16:12 +0200

    import agent classes from one single file to avoid changing imports each time you want to try another agent

---
+++ dopamine/thesis/agents/agents.py
@@ -0,0 +1,3 @@
+from thesis.agents.DQNAgent import DQNAgent
+from thesis.agents.DQVAgent import DQVAgent
+from thesis.agents.DQVMaxAgent import DQVMaxAgent
--- dopamine/thesis/experiments/dqvmax_pong_offline.py
+++ dopamine/thesis/experiments/dqvmax_pong_offline.py
@@ -1,19 +1,20 @@
 import os
+from typing import List
 
 import optax
 from dopamine.jax import losses
-from thesis import config, exploration, patcher
-from thesis.agents import DQVMaxAgent
+from thesis import config, exploration, networks, patcher
+from thesis.agents import agents
 from thesis.memory import offline_memory
 from thesis.reporter import reporter
 from thesis.runner import FixedBatchRunner, runner
 
 # TODO
-# death by max_episode_steps?
+# max_frames_per_episode
 
 
 conv_adam = {
-    "model": {"call_": patcher.NatureDQNNetwork},
+    "model": {"call_": networks.NatureDQNNetwork},
     # "model": {"hiddens": (512, 512)},
     "optim": {"call_": optax.adam, "learning_rate": 0.001, "eps": 3.125e-4},
     "loss_metric": losses.huber_loss,
@@ -25,19 +26,20 @@ conf_pong_dqvmax_offline = lambda exp_name: {
     "nets": {"qfunc": conv_adam, "vfunc": conv_adam},
     "exploration": {"call_": exploration.egreedy, "epsilon_eval": 0.001},
     "agent": {
-        "call_": DQVMaxAgent.DQVMaxAgent,
+        "call_": agents.DQVMaxAgent,
         "net_sync_freq": 2000,
-        "train_freq": 4,
+        # "train_freq": 4,
         # "clip_rewards": True,
         # "min_replay_history": 2000
     },
     "memory": {
         "call_": offline_memory.OfflineOutOfGraphReplayBuffer,
         "batch_size": 256,
+        # "load_parallel": False,
     },
     "env": {
         "call_": patcher.create_atari_environment,
-        "environment_name": "Pong",
+        "environment_name": "ALE/Pong",
         "version": "v5",
         # "environment_name": "CartPole",
         # "version": "v1",
@@ -65,29 +67,35 @@ conf_pong_dqvmax_offline = lambda exp_name: {
         },
         "aim": {
             "call_": reporter.AimReporter,
-            # "repo": str(config.data_dir),
-            "repo": str(config.scratch_data_dir),
+            "repo": str(config.data_dir),
+            # "repo": str(config.scratch_data_dir),
         },
     },
 }
 
+
 # sample 10 replay buffers, 20% of DQN replay dataset
 # (for max only like 1 or 2)
-conf = conf_pong_dqvmax_offline("test_pong_dqvmax_off")
-redundancy = 5
-expanded_confs = runner.add_redundancies(conf, redundancy)
-pong_dqn_buffers_dir = os.path.join(config.data_dir, "Pong")
-expanded_confs = runner.add_offline_buffers(
-    expanded_confs,
-    pong_dqn_buffers_dir,
-    intermediate_dirs="replay_logs",
-    iterations=[1],
-)
-conf_0, *rest_confs = expanded_confs
-from thesis import utils
+def make_configs(
+    exp_name: str, repeats: int, iterations: List[List[int]]
+) -> List[dict]:
+    return runner.add_offline_buffers(
+        runner.add_redundancies(conf_pong_dqvmax_offline(exp_name), repeats),
+        os.path.join(config.data_dir, "Pong"),
+        intermediate_dirs="replay_logs",
+        iterations=iterations,
+    )
 
-utils.data_dir_from_conf(
-    conf_0["experiment_name"], conf_0, basedir=config.scratch_data_dir
-)
-run = runner.create_runner(conf_0)
+
+confs = make_configs("test_pong_dqvmax_off", 5, [[1]])
+# runner.p_run_experiments(confs)
+
+
+# conf0, *rest_confs = expanded_confs
+# from thesis import utils
+
+# utils.data_dir_from_conf(
+#     conf0["experiment_name"], conf0, basedir=config.scratch_data_dir
+# )
+# run = runner.create_runner(conf0)
 # run.run_experiment()
