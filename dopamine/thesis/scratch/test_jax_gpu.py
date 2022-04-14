84baa897690bf6e1b327c4874c582793eed75da7
xqz-u <marco.gallo0530@gmail.com>
2022-04-14 19:08:35 +0200

    execution times on peregrine, cpu and gpu nodes

--- dopamine/thesis/scratch/test_jax_gpu.py
+++ dopamine/thesis/scratch/test_jax_gpu.py
@@ -14,9 +14,13 @@ y = np.random.normal(size=(size, size)).astype(np.float32)
 y = device_put(y)
 """
 # stern: 32.80970892100595
+# peregrine: 8.99069713299832
+# peregrine gpu: 1.5610818099230528
 exec_time = timeit.timeit("jnp.dot(x, x.T).block_until_ready()", setup=code, number=100)
 print(f"exec time: {exec_time}")
 
 # stern: 32.76028992999636
+# peregrine: 9.075912225998763
+# peregrine gpu: 0.6199215138331056
 exec_time = timeit.timeit("jnp.dot(y, y.T).block_until_ready()", setup=code, number=100)
 print(f"exec time: {exec_time}")
