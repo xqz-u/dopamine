2fcaa84546287154057420c03e1c0f2d1db11439
xqz-u <marco.gallo0530@gmail.com>
2022-04-20 19:00:42 +0200

    thesis env on alien environment

--- peregrine/activate_thesis_env.sh
+++ peregrine/activate_thesis_env.sh
@@ -2,10 +2,14 @@
 # NOTE source this file, as it changes the $PATH
 
 modules=('git/2.33.1-GCCcore-11.2.0-nodocs'
-	 'Python/3.9.6-GCCcore-11.2.0')
+	 'Python/3.9.6-GCCcore-11.2.0'
+	 'cuDNN/8.0.4.30-CUDA-11.1.1')
 
 for m in "${modules[@]}"
 do
     module load "$m"
     echo "Loaded module $m"
 done
+
+cd $HOME/thesis
+poetry shell
