a611d13c622bf4cd128455379ca9a3661beb33e0
xqz-u <marco.gallo0530@gmail.com>
2022-04-20 11:39:24 +0200

    write mongo logs to file on peregrine

--- peregrine/mongo_singularity.sh
+++ peregrine/mongo_singularity.sh
@@ -1,10 +1,19 @@
 #! /usr/bin/bash
 
+# TODO pass a config to mongo singularity container:
+# - increase snapshot time (2m rn?)
+
 cd /data/$USER
 
 # NOTE to install the setup:
 # echo 'export SINGULARITY_CACHEDIR=/data/$USER/.singularity' >> ~/.bashrc
 # singularity pull docker://mongo
-# mkdir mongodb
+# mkdir mongodb mongodb_logs
 
-singularity instance start --bind mongodb:/data/db mongo_latest.sif mongodb
+# singularity run --bind mongodb:/data/db mongo_latest.sif # foreground
+singularity instance start \
+	    -B mongodb:/data/db \
+	    -B $HOME/thesis/resources/data/mongodb/mongoconf:/etc/mongo/ \
+	    -B mongodb_logs:/var/log/mongodb \
+	    mongo_latest.sif mongodb
+singularity run instance://mongodb
