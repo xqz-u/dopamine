4c3e8b65abeed3c70a3e3d71eb4e0c1aead6f8c3
xqz-u <marco.gallo0530@gmail.com>
2022-04-20 16:12:36 +0200

    proper mongo singularity setup, using own def file to change starting command and give --config

--- peregrine/mongo_singularity.sh
+++ peregrine/mongo_singularity.sh
@@ -1,19 +1,20 @@
 #! /usr/bin/bash
 
-# TODO pass a config to mongo singularity container:
-# - increase snapshot time (2m rn?)
+# TODO mongod config:
+# - increase snapshot time (~2m rn?)
+
+# NOTE the singularity image should be present on the host; it can
+# be built locally with:
+# sudo singularity build mongo_latest.sif ../resources/data/mongodb/Singularity
 
 cd /data/$USER
-
-# NOTE to install the setup:
+# to install the setup:
 # echo 'export SINGULARITY_CACHEDIR=/data/$USER/.singularity' >> ~/.bashrc
 # singularity pull docker://mongo
 # mkdir mongodb mongodb_logs
 
-# singularity run --bind mongodb:/data/db mongo_latest.sif # foreground
 singularity instance start \
 	    -B mongodb:/data/db \
 	    -B $HOME/thesis/resources/data/mongodb/mongoconf:/etc/mongo/ \
 	    -B mongodb_logs:/var/log/mongodb \
 	    mongo_latest.sif mongodb
-singularity run instance://mongodb
