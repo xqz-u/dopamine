a874f4f1b656b9d718ef52754a0b1385a5eaea52
xqz-u <marco.gallo0530@gmail.com>
2022-04-20 11:41:33 +0200

    little mistakes

--- peregrine/load_modules.sh
+++ peregrine/load_modules.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 # NOTE source this file, as it changes the $PATH
 
-modules=('/software/software/git/2.33.1-GCCcore-11.2.0-nodocs/bin'
+modules=('git/2.33.1-GCCcore-11.2.0-nodocs'
 	 'Python/3.9.6-GCCcore-11.2.0')
 
 for m in "${modules[@]}"
