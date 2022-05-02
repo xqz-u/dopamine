941e91b27f20487ff50dd82d883839f0d90525c1
xqz-u <marco.gallo0530@gmail.com>
2022-05-02 19:29:37 +0200

    time atari train as well

--- dopamine/thesis/experiments/pg_time_train_iter_atari.py
+++ dopamine/thesis/experiments/pg_time_train_iter_atari.py
@@ -8,8 +8,8 @@ from thesis.runner import runner
 make_conf = lambda exp_name: {
     "experiment_name": exp_name,
     "nets": {
-        "qfunc": config.classic_control_mlp_huberloss_adam,
-        "vfunc": config.classic_control_mlp_huberloss_adam,
+        "qfunc": config.convnet_huberloss_adam,
+        "vfunc": config.convnet_huberloss_adam,
     },
     "exploration": config.egreedy_exploration,
     "agent": config.make_batch_rl_agent(agents.DQVMaxAgent),
@@ -29,7 +29,7 @@ make_conf = lambda exp_name: {
 
 
 def main():
-    conf = make_conf("pippo")
+    conf = make_conf("time_train_atari")
     conf, *_ = runner.expand_conf(
         conf,
         1,
@@ -43,13 +43,12 @@ def main():
     )
     run = runner.create_runner(conf)
     start = time.time()
-    # run.run_experiment()
-    run.agent.learn()
+    run.run_experiment()
     print(f"train iteration exec time: {time.time() - start}")
 
 
 def main_pg():
-    conf = make_conf("pippo")
+    conf = make_conf("time_train_atari_pg")
     conf, *_ = runner.expand_conf(
         conf,
         1,
@@ -63,8 +62,7 @@ def main_pg():
     )
     run = runner.create_runner(conf)
     start = time.time()
-    # run.run_experiment()
-    run.agent.learn()
+    run.run_experiment()
     print(f"train iteration exec time: {time.time() - start}")
 
 
