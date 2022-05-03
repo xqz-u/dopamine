43d3da2500d7e9db6fe652279bdee9c3c262798e
xqz-u <marco.gallo0530@gmail.com>
2022-05-03 17:42:42 +0200

    simple experiments on peregrine with prioritized memory

--- dopamine/thesis/experiments/cp_ab_dqvmax_distr_shift.py
+++ dopamine/thesis/experiments/cp_ab_dqvmax_distr_shift.py
@@ -1,36 +1,79 @@
 import os
 
-from thesis import config, offline_circular_replay_buffer
-from thesis.agents import DQVMaxAgent
-from thesis.experiments.cp_ab_dqn_full_experience import make_config
+from thesis import config
+from thesis.experiments import pg_time_train_iter_cc
+from thesis.memory import prio_offline_memory
 from thesis.runner import runner
 
-dqn_full_exp_conf = lambda exp_name, full_env: {
-    "call_": offline_circular_replay_buffer.OfflineOutOfGraphReplayBuffer,
-    "_buffers_root_dir": os.path.join(
-        config.data_dir, f"{full_env}/DQNAgent/{exp_name}/checkpoints/full_experience"
-    ),
-}
+
+def buildconf(exp_name: str, env: str, version: str) -> dict:
+    conf = pg_time_train_iter_cc.make_conf(exp_name)
+    conf["env"] = config.make_env(env, version)
+    conf["runner"]["experiment"] = {
+        "schedule": "train_and_eval",
+        "seed": 4,
+        "steps": int(1e3),
+        "iterations": 500,
+        "eval_period": 2,
+    }
+    return conf
 
 
-def main(exp_name, env, version, full_experience_exp_name):
-    conf = make_config(exp_name, env, version)
-    conf["nets"]["vfunc"] = conf["nets"]["qfunc"]
-    conf["agent"]["call_"] = DQVMaxAgent.DQVMaxAgent
-    conf["runner"]["call_"] = runner.FixedBatchRunner
-    conf["memory"] = dqn_full_exp_conf(full_experience_exp_name, f"{env}-{version}")
-    conf["runner"]["experiment"].pop("record_experience", None)
-    conf["runner"]["experiment"]["schedule"] = "train_and_eval"
-    conf["runner"]["experiment"]["eval_period"] = 2
-    runner.run_experiment([conf, 0])
-    # conf["runner"]["experiment"]["iterations"] = 2
-    # from thesis import utils
+def buildconf_priority(exp_name: str, env: str, version: str) -> dict:
+    conf = buildconf(exp_name, env, version)
+    conf["memory"][
+        "call_"
+    ] = prio_offline_memory.PrioritizedOfflineOutOfGraphReplayBuffer
+    return conf
 
-    # utils.data_dir_from_conf(exp_name, conf)
-    # return runner.create_runner(conf)
+
+def make_confs(
+    exp_name: str,
+    env: str,
+    version: str,
+    env_folder: str,
+    data_basedir: str,
+    conf_builder: callable = buildconf,
+):
+    return runner.expand_conf(
+        conf_builder(exp_name, env, version),
+        3,
+        buffers_root_dir=os.path.join(
+            data_basedir,
+            f"{env}-{version}/DQNAgent/{env_folder}_dqn_full_experience_%%/checkpoints/full_experience",
+        ),
+    )
+
+
+def classic_confs() -> list:
+    return [
+        make_confs("cp_dqvmax_distr_shift", "CartPole", "v1", "cp", config.data_dir),
+        make_confs("ab_dqvmax_distr_shift", "Acrobot", "v1", "ab", config.data_dir),
+    ]
+
+
+def priority_confs_pg() -> list:
+    return [
+        make_confs(
+            "cp_dqvmax_distr_shift_prio",
+            "CartPole",
+            "v1",
+            "cp",
+            config.peregrine_data_dir,
+            conf_builder=buildconf_priority,
+        ),
+        make_confs(
+            "ab_dqvmax_distr_shift_prio",
+            "Acrobot",
+            "v1",
+            "ab",
+            config.peregrine_data_dir,
+            conf_builder=buildconf_priority,
+        ),
+    ]
 
 
 if __name__ == "__main__":
-    pass
-    # main("cp_dqvmax_distr_shift", "CartPole", "v1", "cp_dqn_full_experience_%%")
-    # main("ab_dqvmax_distr_shift", "Acrobot", "v1", "ab_dqn_full_experience_%%")
+    # confs = classic_confs()
+    confs = priority_confs_pg()
+    runner.p_run_experiments([c for c_env in confs for c in c_env])
