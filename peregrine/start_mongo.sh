a0dbcde69a6834f5948489f0268c1c3408c3a17c
xqz-u <marco.gallo0530@gmail.com>
2022-05-20 17:52:21 +0200

    one way to properly use mongo on pg

--- peregrine/start_mongo.sh
+++ peregrine/start_mongo.sh
@@ -7,6 +7,12 @@
 # be built locally with:
 # sudo singularity build mongo_latest.sif ../resources/data/mongodb/Singularity
 
+# NOTE very important: close any running mongodb instance on the
+# interactive node before submitting a job! since they use the same db
+# - same files - only one can run at a time. so either start the db on
+# a compute node and connect to it at localhost:27017, or find a way to
+# connect to the interactive node from the compute node
+
 cd /data/$USER
 # to install the setup:
 # echo 'export SINGULARITY_CACHEDIR=/data/$USER/.singularity' >> ~/.bashrc
