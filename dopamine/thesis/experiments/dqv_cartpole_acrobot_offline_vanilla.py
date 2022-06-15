9ca5290c3678a3fb63dd33495fad03287b68f231
xqz-u <marco.gallo0530@gmail.com>
2022-06-16 01:54:05 +0200

    run forrest run DQV

--- dopamine/thesis/experiments/dqv_cartpole_acrobot_offline_vanilla.py
+++ dopamine/thesis/experiments/dqv_cartpole_acrobot_offline_vanilla.py
@@ -1,4 +1,5 @@
 import os
+import pprint
 
 from thesis import (
     agent,
@@ -13,52 +14,61 @@ from thesis import (
     utils,
 )
 from thesis.agent import utils as agent_utils
-from thesis.experiments import dqv_cartpole_acrobot_offline_gin
+from thesis.scratch import dqv_cartpole_acrobot_offline_gin
 
 utils.setup_root_logging()
 
 
 # NOTE loads all replay buffers iterations
-def make_runner(
+def make_runner_conf(
     experiment_name: str,
     env_name: str,
     seed: int,
     offline_data_dir: str,
     redundancy: int,
     logs_base_dir: str,
-    iterations: int = 500,
-    steps: int = 1000,
+    iterations: int = 1000,
+    steps: int = 600,
 ) -> runner.Runner:
     env = instantiators.create_gym_environment(*env_name.split("-"))
     logs_dir = utils.data_dir_from_conf(
         experiment_name, env_name, agent.DQV.__name__, logs_base_dir
     )
-    make_model_def = lambda features: instantiators.create_model_TS_def(
+    make_model_def = lambda features, env_name: agent_utils.ModelDefStore(
         **{
-            "model_def": (
+            "net_def":
+            # (
+            #     networks.EnsembledNet,
+            #     {
+            #         "n_heads": 3,
+            #         "model":
+            (
                 networks.MLP,
                 {"features": features, **constants.env_preproc_info[env_name]},
             ),
-            "optimizer": instantiators.adam(
-                **{
-                    "learning_rate": 0.001,
-                    "eps": 3.125e-4,
-                }
-            ),
+            #     },
+            # )
+            "opt": instantiators.adam,
+            "opt_params": {
+                "learning_rate": 0.001,
+                "eps": 3.125e-4,
+            },
             "loss_fn": instantiators.mse_loss,
         }
     )
-    # run = runner.FixedBatchRunner(
     return {
         "iterations": iterations,
         "steps": steps,
         "eval_steps": steps,
         "eval_period": 5,
         "redundancy": redundancy,
+        "experiment_name": experiment_name,
         "env": env,
         "checkpoint_base_dir": logs_dir,
         "reporters": [
-            reporter.MongoReporter(experiment_name=experiment_name),
+            reporter.MongoReporter(
+                experiment_name=experiment_name, metrics_buffering=4
+            ),
             reporter.AimReporter(
                 experiment_name=experiment_name,
                 repo=str(logs_base_dir),
@@ -77,15 +87,13 @@ def make_runner(
                         "replay_capacity": steps * iterations,
                         "batch_size": 32,
                         "buffers_dir": os.path.join(offline_data_dir, str(redundancy)),
-                    }
+                    },
                 ),
-                "Q_model_def": make_model_def(2),
-                "V_model_def": make_model_def(1),
+                "Q_model_def": make_model_def(2, env_name),
+                "V_model_def": make_model_def(1, env_name),
             }
         ),
     }
-    # )
-    # run.run()
 
 
 args = []
@@ -97,8 +105,8 @@ for i in range(dqv_cartpole_acrobot_offline_gin.REDUNDANCY):
             dqv_cartpole_acrobot_offline_gin.CARTPOLE_START_SEED + i,
             dqv_cartpole_acrobot_offline_gin.dqn_cartpole_replay_buffers_root,
             i,
-            # constants.data_dir,
-            constants.scratch_data_dir,
+            constants.data_dir,
+            # constants.scratch_data_dir,
         ]
     )
     args.append(
@@ -108,43 +116,27 @@ for i in range(dqv_cartpole_acrobot_offline_gin.REDUNDANCY):
             dqv_cartpole_acrobot_offline_gin.ACROBOT_START_SEED + i,
             dqv_cartpole_acrobot_offline_gin.dqn_acrobot_replay_buffers_root,
             i,
-            # constants.data_dir,
-            constants.scratch_data_dir,
+            constants.data_dir,
+            # constants.scratch_data_dir,
         ]
     )
 
-if False:
-    import multiprocessing as mp
 
-    with mp.Pool(processes=len(args)) as pool:
-        pool.starmap(make_runner, args)
-
-
-aconf = make_runner(*args[0])
-run = runner.FixedBatchRunner(**aconf)
-
-
-def callable_name_getter(call_: callable):
-    return getattr(call_, "__name__", type(call_).__name__)
-
-
-def config_collector(obj: object, conf_dict: dict) -> dict:
-    if not hasattr(obj, "reportable"):
-        return conf_dict
-    for field in obj.reportable:
-        value = getattr(obj, field)
-        if hasattr(value, "reportable"):
-            conf_dict[field] = {
-                "call_": callable_name_getter(value),
-                **config_collector(value, {}),
-            }
-        else:
-            conf_dict[field] = value
-    return conf_dict
+def run_once(conf_args):
+    run = runner.FixedBatchRunner(**make_runner_conf(*conf_args))
+    c = utils.reportable_config(
+        {
+            "call_": utils.callable_name_getter(run),
+            **utils.config_collector(run, "reportable"),
+        }
+    )
+    pprint.pprint(c)
+    for rep in run.reporters:
+        rep.register_conf(c)
+    run.run()
 
 
-c = {}
-c["agent"] = {
-    "call_": callable_name_getter(run.agent),
-    **config_collector(run.agent, {}),
-}
+import multiprocessing as mp
+
+with mp.Pool() as pool:
+    pool.map(run_once, [arg for arg in args])
