6c4621b861304363a29552ef7f7bd25f35bb7455
xqz-u <marco.gallo0530@gmail.com>
2022-06-19 12:38:19 +0200

    gather new experience for the usual 2 envs again before running with ensembles

---
+++ dopamine/thesis/experiments/dqn_cartole_acrobot_full_experience.py
@@ -0,0 +1,46 @@
+from thesis import utils
+
+utils.setup_root_logging()
+
+from thesis import agent, configs, constants, experiments, runner
+
+exp_name_fn = (
+    lambda ag, env, prefix="": f"{prefix}{utils.callable_name_getter(ag)}_{env}_full_experience"
+)
+
+agents_and_models = [(agent.DQN, configs.dqn_model_maker)]
+envs = ["CartPole-v1", "Acrobot-v1"]
+
+confs = [
+    c
+    for agent_class, model_maker in agents_and_models
+    for env_name in envs
+    for c in [
+        {
+            "seed": experiments.DEFAULT_SEED + i,
+            "redundancy": i,
+            "agent_class": agent_class,
+            "env_name": env_name,
+            # "experiment_name": exp_name_fn(agent_class, env_name, prefix="fake_"),
+            "experiment_name": exp_name_fn(agent_class, env_name),
+            "model_maker_fn": model_maker,
+            "logs_base_dir": constants.data_dir,
+            # "logs_base_dir": constants.scratch_data_dir,
+            "experiment": {
+                "iterations": 1000,
+                "steps": 500,
+                "eval_steps": 500,
+                "eval_period": 50,
+                "record_experience": True,
+            },
+            # "reporters": {"mongo": {"metrics_buffering": 1}},
+            # "agent": {"min_replay_history": 100},
+        }
+        for i in range(experiments.DEFAULT_REDUNDANCY)
+    ]
+]
+EXPERIMENT_NAMES = [c["experiment_name"] for c in confs]
+
+
+if __name__ == "__main__":
+    runner.run_parallel(confs, runner.OnlineRunner)
