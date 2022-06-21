79d34f47ffc6402a2203b97a1cb7cc00e5ee88cd
xqz-u <s3680622@pg-gpu>
2022-06-21 18:44:21 +0200

    trying other version of jaxlib to avoid errors

--- peregrine/activate_thesis_env.sh
+++ peregrine/activate_thesis_env.sh
@@ -3,7 +3,9 @@
 
 modules=('git/2.33.1-GCCcore-11.2.0-nodocs'
 	 'Python/3.9.6-GCCcore-11.2.0'
-	 'cuDNN/8.0.4.30-CUDA-11.1.1')
+	 # 'cuDNN/8.0.4.30-CUDA-11.1.1'
+	 'cuDNN/8.2.1.32-CUDA-11.3.1'
+	)
 
 for m in "${modules[@]}"
 do
