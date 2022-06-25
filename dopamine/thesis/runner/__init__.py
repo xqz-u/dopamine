7b11848f67b5ccea2d0a9679370d32205f0f4cd2
xqz-u <marco.gallo0530@gmail.com>
2022-06-25 17:18:46 +0200

    some runs were going missing in mongo and aim when a lot were mapped over

--- dopamine/thesis/runner/__init__.py
+++ dopamine/thesis/runner/__init__.py
@@ -1,4 +1,6 @@
+import logging
 import multiprocessing as mp
+import time
 from pprint import pprint
 from typing import Any, List
 
@@ -7,8 +9,14 @@ from thesis.runner.base import Runner
 from thesis.runner.fixed_batch_runner import FixedBatchRunner
 from thesis.runner.online_runner import OnlineRunner
 
+logger = logging.getLogger(__name__)
 
-def run_experiment(conf: dict):
+
+def run_experiment(conf: dict, wait_time: float = None):
+    print(conf, wait_time)
+    if wait_time:
+        logger.info(f"Sleep {wait_time} seconds...")
+        time.sleep(wait_time)
     runner_class = conf.pop("runner")
     run = runner_class(**experiments.make_conf(**conf))
     c = utils.reportable_config(
@@ -27,5 +35,5 @@ def run_experiment(conf: dict):
 # should instead be saved inside confs
 def run_parallel(confs: List[dict]) -> List[Any]:
     with mp.Pool() as pool:
-        res = pool.map(run_experiment, confs)
+        res = pool.starmap(run_experiment, zip(confs, range(len(confs))))
     return res
